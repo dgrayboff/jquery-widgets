d38c1cecc0307839ddc257bfdfc7f16832d7b4b6
David <dgrayboff@gmail.com>
2016-05-31 22:20:29 -0400

    initial commit

---
+++ textarea/assets/textarea.js
@@ -0,0 +1,21 @@
+ //
+ // var maxLength = 10;
+ //
+ // $('textarea').keyup(function() {
+ //   var length = $(this).val().length;
+ //   var length = maxLength - length;
+ // });
+ //
+ // $('#chars').text(length);
+
+
+
+var maxchars = 25;
+
+$('textarea').keyup(function () {
+    var charLength = $('textarea').val().length;
+    $('textarea').val($('textarea').val().substring(0, maxchars));
+    var charLength = $('textarea').val().length;
+    remain = maxchars - parseInt(charLength);
+    $('#remain').text(remain);
+});
